178f6fa852d561a6b18ec3d3bbbeb8ea7d663e59
Ros Jason Salgado <rjsalgado@besixgroup.com>
2022-08-13 23:05:01 +0800

    Initial Code

---
+++ LoginAuth/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using LoginAuth.Models;
+using LoginAuth.Models.Constants;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IConfiguration _config;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Login(LoginViewModel model)
+        {
+            var user = Authenticate(model);
+            var token = string.Empty;
+
+            if (user != null)
+                token = Generate(user);
+
+            return Json(token);
+        }
+
+        private UserModel Authenticate(LoginViewModel model)
+        {
+            var currentUser = Users.UserData.FirstOrDefault(x => x.Username == model.Username && x.Password == model.Password);
+
+            if (currentUser != null)
+                return currentUser;
+
+            return null;
+        }
+
+        private string Generate(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Email, user.EmailAddress),
+                new Claim(ClaimTypes.GivenName, user.GivenName),
+                new Claim(ClaimTypes.Surname, user.Surname),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"]
+                , _config["Jwt:Audience"]
+                , claims
+                , expires: DateTime.Now.AddMinutes(1)
+                , signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        #region Default
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        #endregion
+    }
+}
---
+++ LoginAuth/Models/Constants/Users.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LoginAuth.Models.Constants
+{
+    public class Users
+    {
+        public static List<UserModel> UserData = new List<UserModel>
+        {
+            new UserModel { Username = "user"
+                , EmailAddress = "user@gmail.com"
+                , GivenName = "user"
+                , Password = "user"
+                , Surname = "reg"
+                , Role = "Regular" },
+
+            new UserModel { Username = "admin"
+                , EmailAddress = "admin@gmail.com"
+                , GivenName = "admin"
+                , Password = "admin"
+                , Surname = "admin"
+                , Role = "Administrator" }
+        };
+    }
+}
